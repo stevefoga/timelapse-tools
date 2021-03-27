cae923e12cf3abe7988aa6804e65d81081241b4a
Steve Foga <steve.foga@gmail.com>
2021-03-27 16:23:48 -0500

    extension retries; copy option default; jpg default extension

--- utils/batch_rename.py
+++ utils/batch_rename.py
@@ -1,4 +1,4 @@
-'''
+"""
 batch_rename.py
 
 Purpose: remove special characters that are inserted by the timelapse cam.
@@ -6,16 +6,36 @@ Purpose: remove special characters that are inserted by the timelapse cam.
 Author:     Steve Foga
 Created:    18 November 2017
 
-Python version: 2.7.10
-'''
+Python version: 3.9.2
+"""
 import os
 import shutil
 import glob
 
-def main(src, dst=None, dryrun=False, renumber=False):
+DEFAULT_EXTENSION = "jpg"
+
+
+def get_sorted_images(src, ext):
+    """
+
+    :param src: <str>
+    :param ext: <str>
+    :return: <list>
+    """
+    return sorted(glob.glob(os.path.join(src, '*.{}'.format(ext))))
+
+
+def batch_rename(src, extension=DEFAULT_EXTENSION, dst=None, move=False, renumber=False, dryrun=False):
 
     print("dst: {0}".format(dst))
-    fn_in = sorted(glob.glob(os.path.join(src,'*.jpg')))
+    fn_in = get_sorted_images(src, extension)
+    if not fn_in:
+        fn_in = get_sorted_images(src, extension.upper())
+        if not fn_in:
+            fn_in = get_sorted_images(src, extension.lower())
+    if not fn_in:
+        raise Exception(f"No input images found in '{src}' with extensions '{extension}' "
+                        f"(also tried '{extension.upper()}' and '{extension.lower()}')")
 
     if renumber:
         img_ct = 0
@@ -43,9 +63,12 @@ def main(src, dst=None, dryrun=False, renumber=False):
             fn_out = os.path.join(os.path.dirname(f), fout)
 
         if dryrun:
-            print("Dryrun: move {0} to {1}".format(f, fn_out))
+            print("Dryrun: copy {0} to {1}".format(f, fn_out))
         else:
-            shutil.move(f, fn_out)
+            if move:
+                shutil.move(f, fn_out)
+            else:
+                shutil.copy2(f, fn_out)  # use .copy2() to preserve metadata
 
 
 if __name__ == "__main__":
@@ -57,11 +80,16 @@ if __name__ == "__main__":
 
     req_named.add_argument("src", help="Dir of input images; will overwrite files if -o not specified.")
 
+    parser.add_argument("-e", "--ext",
+                        help=f"File extension (will attempt lowercase and UPPERCASE instances of this string "
+                             f"if the input fails (defaut={DEFAULT_EXTENSION})", dest="extension",
+                        default=DEFAULT_EXTENSION)
     parser.add_argument("-o", help="Output dir", dest="dst", required=False)
     parser.add_argument("--renumber", help="Rename files to sequential numbers by alphanumeric order",
                         action="store_true", required=False)
+    parser.add_argument("-m", "--move", help="Move files instead of making a copy", action="store_true", required=False)
     parser.add_argument("--dryrun", help="Run script, but do not execute actions", action="store_true", required=False)
 
     arguments = parser.parse_args()
 
-    main(**vars(arguments))
+    batch_rename(**vars(arguments))
