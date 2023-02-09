002efe88f2260a5283d9a3d0429694eb28db068a
Steve Foga <steve.foga@gmail.com>
2023-02-08 21:04:41 -0500

    fix renaming bug

--- scripts/default_timelapse_recentimage_5sec.sh
+++ scripts/default_timelapse_recentimage_5sec.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 # defaults
-OUTPUT_IMG=/home/pi/capture/$(date '+%Y%m%d%H%M%S').jpg;
 RECENT_IMG=/home/pi/recent/most_recent.jpg;
 
 # use while loop to control caputure rate
 while true; do
+  # set output image
+  OUTPUT_IMG=/home/pi/capture/$(date '+%Y%m%d%H%M%S').jpg;
+  
   # capture
   #sudo fswebcam --no-banner -d /dev/video0 -r 2304x1536 -s brightness=50% -p YUYV -S 60 -s backlight_compensation=1 -s sharpness=150 -s focus_auto=0 "$OUTPUT_IMG";
   sudo fswebcam -d /dev/video0 -r 2304x1536 -s brightness=50% -p YUYV -s backlight_compensation=1 -s sharpness=150 -s focus_auto=1 "$OUTPUT_IMG";
 
-  # plaace freshly captured image in 'recent' dir
+  # place freshly captured image in 'recent' dir
   if [ -f "$RECENT_IMG" ]; then
     rm -f "$RECENT_IMG";
   fi
